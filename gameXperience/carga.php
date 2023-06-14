f287a423e5cba3d0cc985e0acb6e381e9affb9d3
Royel2000 <113474109+Royel2000@users.noreply.github.com>
2023-06-14 16:36:42 -0500

    14-06-23_4:36

---
+++ gameXperience/carga.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/pagina-carga.css">
+</head>
+
+<body>
+    <div class="contenedor">
+        <div>
+            <img src="imagenes/logo.gif">
+        </div>
+    </div>
+</body>
+
+</html>
