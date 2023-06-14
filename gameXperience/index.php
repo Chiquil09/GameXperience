e06bbd8686d0f7911b47fdcc7e6cbec576ae8323
Royel2000 <113474109+Royel2000@users.noreply.github.com>
2023-06-14 16:36:42 -0500

    14-06-23_4:36

---
+++ gameXperience/index.php
@@ -0,0 +1,67 @@
+<?php include_once 'includes/templates/header.php'; ?>
+<section class="home-section">
+    <main class="container">
+        <div class="h-25 p-5">
+            <div id="myCarousel" class="carousel slide mb-6" data-bs-ride="carousel" data-bs-theme="light">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+                            <rect width="100%" height="100%" fill="var(--bs-secondary-color)" />
+                        </svg>
+                        <div class="container">
+                            <div class="carousel-caption text-start">
+
+                                <h1>Primer juego de la serie</h1>
+                                <p class="opacity-75">Some representative placeholder content for the first
+                                    slide of the carousel.</p>
+                                <p><a class="btn btn-lg btn-primary" href="#">Comprar</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+                            <rect width="100%" height="100%" fill="var(--bs-secondary-color)" />
+                        </svg>
+                        <div class="container">
+                            <div class="carousel-caption">
+                                <h1>Segundo Juego de la Serie</h1>
+                                <p>Some representative placeholder content for the second slide of the carousel.
+                                </p>
+                                <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+                            <rect width="100%" height="100%" fill="var(--bs-secondary-color)" />
+                        </svg>
+                        <div class="container">
+                            <div class="carousel-caption text-end">
+                                <h1>One more for good measure.</h1>
+                                <p>Some representative placeholder content for the third slide of this carousel.
+                                </p>
+                                <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+        <div class="container">
+
+        </div>
+    </main>
+    <?php include_once 'includes/templates/footer.php'; ?>
