ad543fd24c6c72a08cd8df1df4a96959964fdfd5
Royel2000 <113474109+Royel2000@users.noreply.github.com>
2023-06-15 23:55:40 -0500

    15-06-11:54
    correccion del carrusel, implementacion de tarjetas para los juegos

--- gameXperience/index.php
+++ gameXperience/index.php
@@ -1,8 +1,8 @@
-<?php include_once 'includes/templates/header.php'; ?>
+<?php include 'includes/templates/header.php'; ?>
 <section class="home-section">
     <main class="container">
         <div class="h-25 p-5">
-            <div id="myCarousel" class="carousel slide mb-6" data-bs-ride="carousel" data-bs-theme="light">
+            <div id="myCarousel" class="carousel slide mb-6" data-bs-ride="carousel" data-bs-theme="dark">
                 <div class="carousel-indicators">
                     <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                     <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -10,42 +10,32 @@
                 </div>
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
-                            <rect width="100%" height="100%" fill="var(--bs-secondary-color)" />
-                        </svg>
+                        <img src="imagenes/halo-infinity.jpg" class="bd-placeholder-img" width="100%" height="100%">
                         <div class="container">
                             <div class="carousel-caption text-start">
-
-                                <h1>Primer juego de la serie</h1>
-                                <p class="opacity-75">Some representative placeholder content for the first
-                                    slide of the carousel.</p>
-                                <p><a class="btn btn-lg btn-primary" href="#">Comprar</a></p>
+                                <h1 class="color">Juegos Populares</h1>
+                                <p class="opacity-75 color">Tenemos para ti una amplia variedad de los mejores juegos del 2023</p>
+                                <p><a class="btn btn-lg btn-primary" href="#">Ir</a></p>
                             </div>
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
-                            <rect width="100%" height="100%" fill="var(--bs-secondary-color)" />
-                        </svg>
+                        <img src="imagenes/assasincreed.jpg" class="bd-placeholder-img" width="100%" height="100%">
                         <div class="container">
                             <div class="carousel-caption">
-                                <h1>Segundo Juego de la Serie</h1>
-                                <p>Some representative placeholder content for the second slide of the carousel.
-                                </p>
-                                <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
+                                <h1 class="color">Accion</h1>
+                                <p class="color">Los mejores juegos de accion</p>
+                                <p><a class="btn btn-lg btn-primary" href="#">Ir</a></p>
                             </div>
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
-                            <rect width="100%" height="100%" fill="var(--bs-secondary-color)" />
-                        </svg>
+                        <img src="imagenes/unreal.jpg" class="bd-placeholder-img" width="100%" height="100%">
                         <div class="container">
                             <div class="carousel-caption text-end">
-                                <h1>One more for good measure.</h1>
-                                <p>Some representative placeholder content for the third slide of this carousel.
-                                </p>
-                                <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+                                <h1 class="color">Mejores Graficos</h1>
+                                <p class="color">Los juegos con graficos ultra realistas</p>
+                                <p><a class="btn btn-lg btn-primary" href="#">Ver</a></p>
                             </div>
                         </div>
                     </div>
@@ -60,8 +50,11 @@
                 </button>
             </div>
         </div>
-        <div class="container">
+        <div>
 
         </div>
+        <div class="container">
+            <?php include 'cards.php'; ?>
+        </div>
     </main>
-    <?php include_once 'includes/templates/footer.php'; ?>
+    <?php include 'includes/templates/footer.php'; ?>
