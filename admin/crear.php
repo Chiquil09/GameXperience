44eaad52c72303e3a7b275563c9bbb0731a2a41f
zurielprogramador <argenis.canton08@gmail.com>
2023-06-20 17:15:38 -0500

    20/06/5:15pm
    avances

---
+++ admin/crear.php
@@ -0,0 +1,52 @@
+<?php
+include("../includes/templates/header.php");
+?>
+<section class="home-section">
+<main class="contenedor container px-5">
+    <h1>Administrador de juegos</h1>
+
+    <a href="admin/propiedades/crear.php" class="boton boton-verde">Nuevo Juego</a>
+    <a href="admin/vendedores/crear.php" class="boton boton-amarillo">Nuevo Vendedor</a>
+    <h2>Juegos</h2>
+   
+    <form>
+        <table width="540" >
+          <tr valign="top">
+            <td width="500">Nombre</td>
+            <td width="414"><input type="text" id="titulo" placeholder="Nombre del juego "></td>
+          </tr>
+          <tr valign="top">
+            <td>Imagen:</td>
+            
+            <td><input type="file"  id="imagen"  accept="image/jpeg, image/png" ></td>
+          </tr>
+          <tr valign="top">
+            <td>Descripcion:</td>
+            <td><textarea name="consulta" cols="130" rows="15"></textarea></td>
+          </tr>
+          <tr valign="top">
+            <td>Poster:</td>
+            
+            <td><input type="file"  id="imagen" accept="image/jpeg, image/png"></td>
+          </tr>
+
+          <tr valign="top">
+            <td width="500">Precio</td>
+            <td width="414"><input type="number" id="titulo" placeholder="Precio del juego "maxlength="15"></td>
+          </tr>
+
+          <tr valign="top">
+            <td width="500">Calificacion</td>
+            <td width="414"><input type="number" id="wc" placeholder="Ej: 5" min="1" max="5" ></td>
+          </tr>
+
+
+        </table>
+        
+          <input name="restablecer" type="reset" id="restablecer" value="Restablecer">
+          <button type="submit" name="enviar" id="enviar">Registrarse</button>
+        
+      
+
+
+        </form>
--- admin/index.php
+++ admin/index.php
@@ -3,11 +3,11 @@ include("../includes/templates/header.php");
 ?>
 <section class="home-section">
 <main class="contenedor container px-5">
-    <h1>Administrador de bienes raices</h1>
+    <h1>Administrador de juegos</h1>
 
-    <a href="admin/propiedades/crear.php" class="boton boton-verde">Nueva Porpiedad</a>
+    <a href="admin/crear.php" class="boton boton-verde">Nuevo Juego</a>
     <a href="admin/vendedores/crear.php" class="boton boton-amarillo">Nuevo Vendedor</a>
-    <h2>Propiedades</h2>
+    <h2>Juegos</h2>
     <table class="propiedades">
         <thead>
             <tr>
