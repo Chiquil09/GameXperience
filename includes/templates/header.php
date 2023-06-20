7b5f899f0378aad6952c105bdb6748de0674e180
Chiquil09 <114046386+Chiquil09@users.noreply.github.com>
2023-06-19 20:43:54 -0500

    19-06-8:43pm

---
+++ includes/templates/header.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html data-bs-theme="dark" data-lt-installed="true" lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body class="bg-dark">
+  <div class="d-f">
+    <div class="sidebar d-flex flex-column flex-shrink-0 p-3">
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+        <svg class="bi pe-none me-2" width="40" height="32">
+          <use xlink:href="" />
+        </svg>
+        <span class="fs-4">GameXperience</span>
+      </a>
+      <br>
+      <br>
+
+      <ul class="nav nav-pills flex-column mb-auto">
+        <li class="nav-item">
+          <a href="/index.php" class="nav-link active" aria-current="page">
+            <i class="bi bi-house"></i>
+            Inicio
+          </a>
+        </li>
+        <li class="bajar">
+          <a href="/buscar.php" class="nav-link active">
+            <i class="bi bi-search"></i>
+            Buscar
+          </a>
+        </li>
+
+        <hr class="custom-hr ">
+
+        <li class="bajar">
+          <a href="/biblioteca.php" class="nav-link active">
+            <i class="bi bi-bookmark-fill"></i>
+            Biblioteca
+          </a>
+        </li>
+
+        <li class="bajar">
+          <a href="#" class="nav-link active">
+            <i class="bi bi-braces"></i>
+            Acerca de
+          </a>
+        </li>
+
+        <li class="bajar">
+          <a href="/iniciarsesion1.php" class="nav-link active">
+            <i class="bi bi-braces"></i>
+            Login
+          </a>
+        </li>
+      </ul>
+      <div class="dropdown">
+      </div>
+    </div>
