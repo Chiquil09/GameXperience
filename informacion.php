379179d74a8cd3659563764f59778733ed305610
Royel2000 <113474109+Royel2000@users.noreply.github.com>
2023-06-20 13:53:11 -0500

    20-06-1:53

---
+++ informacion.php
@@ -0,0 +1,45 @@
+<?php include 'includes/templates/header.php'; ?>
+<section class="home-section">
+    <main>
+        <div>
+            <div class="section-fade-out">
+                <img src="imagenes/Halo-Infinite-en-PC-2021-1.jpg" alt="">
+            </div>
+        </div>
+        <div class="d-flex">
+            <div class="c1 container">
+                <div class="profile">
+                    <img src="imagenes/perfil.jpg">
+                </div>
+                <div class="container d-flex inf">
+                    <h3>Halo</h3>
+                    <h2 class="text-body-secondary">$50</h2>
+                </div>
+                <div class="parrafo">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quo!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi facilis
+                        sapiente necessitatibus recusandae tempora quaerat fuga nam similique
+                        enim, magni adipisci repellendus ipsam temporibus ab vero eveniet!
+                        Perferendis, rerum porro! Lorem ipsum dolor sit amet, consectetur 
+                        adipisicing elit. Magni doloribus aperiam expedita odit, quibusdam 
+                        officiis? Repellendus eum doloremque iusto totam suscipit nihil reprehenderit 
+                        corporis beatae voluptas fugiat. Commodi, impedit ipsam!
+                    </p>
+
+                </div>
+            </div>
+            <div class="c2">
+                <h5>puntuacion:</h5>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 30%;" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <ul class="py-3">
+                    <li class="list-group-item">genero</li>
+                    <li class="list-group-item">clasificacion</li>
+                    <li class="list-group-item">desarrollador</li>
+                    <li class="list-group-item"></li>
+                </ul>
+            </div>
+        </div>
+    </main>
+    <?php include 'includes/templates/footer.php'; ?>
