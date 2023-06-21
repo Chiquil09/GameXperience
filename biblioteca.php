6475203f66317a538dcda25de38b1f7b23bdd272
Royel2000 <113474109+Royel2000@users.noreply.github.com>
2023-06-21 17:50:14 -0500

    a

---
+++ biblioteca.php
@@ -0,0 +1,104 @@
+<?php include 'includes/templates/header.php';?>
+    <section class="home-section">
+        <main class="container">
+            <div class="form-floating mb-3 d-flex p-2">
+                <input type="text" class="form-control" name="formId1" id="formId1" placeholder="">
+                <label for="formId1">escribir</label>
+            </div>
+            <div>
+                <div class="album py-5">
+                    <div class="col">
+                        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                            <div class="col">
+                                <div class="carta shadow-sm">
+                                    <img src="imagenes/Halo-Infinite-en-PC-2021-1.jpg"
+                                        class="bd-placeholder-img card-img-top" width="100%">
+                                    <div class="card-body">
+                                        <p class="card-text">Halo Infinite</p>
+                                        <div class="d-flex justify-content-between align-items-center">
+                                            <div class="btn-group">
+                                                <button type="button" class="btn btn-sm"><a class="btn1" href="link_name"><span>ver</span></a></button>
+                                            </div>
+                                            <small class="text-body-secondary">$50</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="carta shadow-sm">
+                                    <img src="imagenes/789573.jpg" class="bd-placeholder-img card-img-top" width="100%">
+                                    <div class="card-body">
+                                        <p class="card-text">God Of War</p>
+                                        <div class="d-flex justify-content-between align-items-center">
+                                            <div class="btn-group">
+                                                <button type="button" class="btn btn-sm"><a class="btn1" href="link_name"><span>ver</span></a></button>
+                                            </div>
+                                            <small class="text-body-secondary">$50</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="carta shadow-sm">
+                                    <img src="imagenes/1366_2000.jpg" class="bd-placeholder-img card-img-top"
+                                        width="100%">
+                                    <div class="card-body">
+                                        <p class="card-text">Call Of Duty </p>
+                                        <div class="d-flex justify-content-between align-items-center">
+                                            <div class="btn-group">
+                                                <button type="button" class="btn btn-sm"><a class="btn1" href="link_name"><span>ver</span></a></button>
+                                            </div>
+                                            <small class="text-body-secondary">$50</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col">
+                                <div class="carta shadow-sm">
+                                    <img src="imagenes/sabine-securitybreach-portfolioporfolio1.jpg"
+                                        class="bd-placeholder-img card-img-top" width="100%">
+                                    <div class="card-body">
+                                        <p class="card-text">Fnaf Security Breach</p>
+                                        <div class="d-flex justify-content-between align-items-center">
+                                            <div class="btn-group">
+                                                <button type="button" class="btn btn-sm"><a class="btn1" href="link_name"><span>ver</span></a></button>
+                                            </div>
+                                            <small class="text-body-secondary">$50</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="carta shadow-sm">
+                                    <img src="imagenes/5fiapcvdzo8.jpg" class="bd-placeholder-img card-img-top" width="100%">
+                                    <div class="card-body">
+                                        <p class="card-text">ARK Survival</p>
+                                        <div class="d-flex justify-content-between align-items-center">
+                                            <div class="btn-group">
+                                                <button type="button" class="btn btn-sm"><a class="btn1" href="link_name"><span>ver</span></a></button>
+                                            </div>
+                                            <small class="text-body-secondary">$50</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="carta shadow-sm">
+                                    <img src="imagenes/1303642.jpeg" class="bd-placeholder-img card-img-top" width="100%">
+                                    <div class="card-body">
+                                        <p class="card-text">DOOM Eternal</p>
+                                        <div class="d-flex justify-content-between align-items-center">
+                                            <div class="btn-group">
+                                                <button type="button" class="btn btn-sm"><a class="btn1" href="link_name"><span>ver</span></a></button>
+                                            </div>
+                                            <small class="text-body-secondary">$50</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </main>
+<?php include 'includes/templates/footer.php'; ?>
