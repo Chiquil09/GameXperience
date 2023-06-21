e15fc8f9e02c6ed79dc18d0e72a36e723c71cf40
Royel2000 <113474109+Royel2000@users.noreply.github.com>
2023-06-21 18:26:13 -0500

    21-06-6:26

--- cards.php
+++ cards.php
@@ -2,7 +2,7 @@
     <h2 class="pb-2 border-bottom">Juegos</h2>
     <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
         <div class="col">
-            <a href="/informacion.php" class="a">
+            <a href="/informacion.php?id=1" class="a">
                 <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" style="background-image: url(imagenes/halo-infinite-lakeside-i97747.jpg);">
                     <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
                         <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Halo Infinity</h3>
