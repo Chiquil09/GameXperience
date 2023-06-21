f92c34f6424b1262b4e93735512d1fe3a467d88d
Royel2000 <113474109+Royel2000@users.noreply.github.com>
2023-06-21 17:02:36 -0500

    Revert "Merge branch 'principal' of https://github.com/Chiquil09/GameXperience into principal"
    This reverts commit 8d28fb602344d2a4f5b7708364f0e3c613bfe7d2, reversing
    changes made to da0e27b8f926f20013dacff00130b15ab670ce15.

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,7 +1,7 @@
 <?php
-function conectarDB() : mysqli{
 
-    $db =new mysqli("localhost", "root","","gameperience"); //mysqli es la forma O.Objetos
+function conectarDB() : mysqli{
+    $db =new mysqli("localhost", "root","","gamexx"); //mysqli es la forma O.Objetos
     if(!$db){
        echo "Error no se pudo conectar";
        exit; 
