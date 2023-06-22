f9dfc13616678687f5b1e0c81ff0200ceb9ec4a5
Royel2000 <113474109+Royel2000@users.noreply.github.com>
2023-06-22 09:58:44 -0500

    22/06-9:58

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -11,9 +11,12 @@ $auth = $_SESSION["login"] ?? false;
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienes Raices</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>GameXperience</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="/build/css/app.css">
 </head>
 
