77be7ac0a2472ee3f4047ce15649d5bb34953efb
Chiquil09 <114046386+Chiquil09@users.noreply.github.com>
2023-06-22 11:34:14 -0500

    22-06-11:34

--- views/paginas/iconos.php
+++ views/paginas/iconos.php
@@ -1,17 +1,17 @@
 <div class="iconos-nosotros">
     <div class="icono">
-        <img src="build/img/icono1.svg" alt="Icono seguridad" loading="lazy">
+        <img src="build\img\jugador.png" alt="Icono seguridad" loading="lazy">
         <h3>Seguridad</h3>
-        <p>Possimus, suscipit repudiandae. Autem deserunt aliquid deleniti sit minus consectetur obcaecati molestiae dolorem natus dolores reiciendis tempore, explicabo cum nobis laudantium. Voluptates?</p>
+        <p>Gamexperience es una emocionante página dedicada a los amantes de los videojuegos. Con una amplia variedad de contenido y recursos, esta plataforma se posiciona como el destino ideal para los apasionados de los juegos. Desde reseñas y análisis de los últimos lanzamientos hasta noticias y avances exclusivos, Gamexperience ofrece a sus usuarios una experiencia completa y actualizada en el mundo de los videojuegos.</p>
     </div>
     <div class="icono">
-        <img src="build/img/icono2.svg" alt="Icono Precio" loading="lazy">
+        <img src="build\img\bullbasaur.png" alt="Icono Precio" loading="lazy">
         <h3>Precio</h3>
-        <p>Possimus, suscipit repudiandae. Autem deserunt aliquid deleniti sit minus consectetur obcaecati molestiae dolorem natus dolores reiciendis tempore, explicabo cum nobis laudantium. Voluptates?</p>
+        <p>Una de las características más destacadas de Gamexperience es su amplio catálogo de juegos disponibles para diferentes plataformas. Ya sea que prefieras jugar en consolas, PC o dispositivos móviles, esta página tiene cubierto cada gusto y preferencia. Además, Gamexperience se enorgullece de contar con una comunidad activa y comprometida, donde los usuarios pueden compartir sus opiniones, participar en foros de discusión y colaborar en la creación de contenido único.</p>
     </div>
     <div class="icono">
-        <img src="build/img/icono3.svg" alt="Icono Tiempo" loading="lazy">
+        <img src="build\img\videojuego (1).png" alt="Icono Tiempo" loading="lazy">
         <h3>A Tiempo</h3>
-        <p>Possimus, suscipit repudiandae. Autem deserunt aliquid deleniti sit minus consectetur obcaecati molestiae dolorem natus dolores reiciendis tempore, explicabo cum nobis laudantium. Voluptates?</p>
+        <p>Además de ser una plataforma de información y entretenimiento, Gamexperience también ofrece recursos útiles para los jugadores. Desde guías de estrategia y trucos hasta tutoriales y consejos, los usuarios pueden aprovechar al máximo sus juegos favoritos y mejorar sus habilidades. Asimismo, Gamexperience organiza eventos y competiciones que permiten a los jugadores demostrar su destreza y conectarse con otros entusiastas de los videojuegos.</p>
     </div>
 </div>
--- views/paginas/nosotros.php
+++ views/paginas/nosotros.php
@@ -1,29 +1,61 @@
-<main class="contenedor seccion">
-        <h1>Conoce sobre Nosotros</h1>
+        <div class="body1">
+            <section class="k">
+                <img src="build\img\stars.png" id="star">
+                <img src="build\img\moon.png" id="moon">
+                <img src="build\img\mountains_behind.png" id="mountains_behind">
+                <h2 id="text">GAMEXPERIENCE</h2>
+                <a href="#sec" id="btn">Ver mas</a>
+                <img src="build\img\mountains_front.png" id="mountains_front">
+            </section>
+            <div class="sec" id="sec">
+                <div class="cen">
+                    <!-- <h2>Nosotros</h2> -->
+                    <img src="/imagenes/nuestroEquipo.png" class="h22" alt="">
+                </div>
+                <p>GameXperience es una página web dedicada a brindar una experiencia completa y enriquecedora en el mundo de los videojuegos.
+                    Nuestro objetivo principal es proporcionar a los jugadores una plataforma única donde puedan encontrar información, noticias,
+                    reseñas, guías y contenido relacionado con sus juegos favoritos.En GameXperience, nos enorgullece ofrecer una amplia variedad
+                    de contenido para satisfacer las necesidades de jugadores de todos los niveles y preferencias. Nuestro equipo de redactores y
+                    expertos en videojuegos está constantemente actualizando el sitio con las últimas noticias de la industria, avances de juegos,
+                    lanzamientos próximos y eventos destacados.e mantener a nuestros usuarios informados, también nos centramos en proporcionar
+                    reseñas detalladas y objetivas de juegos populares. Nuestros análisis van más allá de la superficie y profundizan en aspectos
+                    como jugabilidad, gráficos, narrativa y calidad general. Creemos que una buena reseña debe ser imparcial y ayudar a los jugadores
+                    a tomar decisiones informadas sobre qué juegos jugar.<br><br>
+                    En GameXperience, también ofrecemos guías y consejos para ayudar a los jugadores a superar desafíos en sus juegos favoritos.
+                    Ya sea que necesites ayuda para completar una misión difícil, desbloquear secretos ocultos o mejorar tus habilidades en un juego
+                    de competición, encontrarás información útil y estrategias útiles en nuestras guías.
+                    Además de todo el contenido informativo, GameXperience también cuenta con una comunidad vibrante donde los
+                    jugadores pueden interactuar entre sí, compartir sus experiencias, discutir temas relacionados con los videojuegos y
+                    participar en eventos especiales. Valoramos la participación activa de nuestros usuarios y fomentamos un entorno amigable y
+                    respetuoso para todos.
+                    En resumen, GameXperience es una página web que busca ofrecer a los jugadores una experiencia completa y enriquecedora en el
+                    mundo de los videojuegos. Nuestro compromiso con la calidad, la información precisa y el contenido diverso nos convierte en el
+                    destino ideal para todos aquellos que deseen sumergirse en la apasionante industria de los videojuegos. ¡Únete a nosotros y
+                    descubre la verdadera esencia de la experiencia de juego!</p>
 
-        <div class="contenido-nosotros">
-            <div class="imagen">
-                <picture>
-                    <source srcset="build/img/nosotros.webp" type="image/webp">
-                    <source srcset="build/img/nosotros.jpg" type="image/jpeg">
-                    <img loading="lazy" src="build/img/nosotros.jpg" alt="Sobre Nosotros">
-                </picture>
-            </div>
-
-            <div class="texto-nosotros">
-                <blockquote>
-                    25 Años de experiencia
-                </blockquote>
-
-                <p>Proin consequat viverra sapien, malesuada tempor tortor feugiat vitae. In dictum felis et nunc aliquet molestie. Proin tristique commodo felis, sed auctor elit auctor pulvinar. Nunc porta, nibh quis convallis sollicitudin, arcu nisl semper mi, vitae sagittis lorem dolor non risus. Vivamus accumsan maximus est, eu mollis mi. Proin id nisl vel odio semper hendrerit. Nunc porta in justo finibus tempor. Suspendisse lobortis dolor quis elit suscipit molestie. Sed condimentum, erat at tempor finibus, urna nisi fermentum est, a dignissim nisi libero vel est. Donec et imperdiet augue. Curabitur malesuada sodales congue. Suspendisse potenti. Ut sit amet convallis nisi.</p>
-
-                <p>Aliquam lectus magna, luctus vel gravida nec, iaculis ut augue. Praesent ac enim lorem. Quisque ac dignissim sem, non condimentum orci. Morbi a iaculis neque, ac euismod felis. Fusce augue quam, fermentum sed turpis nec, hendrerit dapibus ante. Cras mattis laoreet nibh, quis tincidunt odio fermentum vel. Nulla facilisi.</p>
+                    <section class="contenedor seccion">
+            <h1 class="ti">Más Sobre Nosotros</h1>
+            <?php include 'iconos.php'; ?>
+        </section>
             </div>
         </div>
-    </main>
 
-    <section class="contenedor seccion">
-        <h1>Más Sobre Nosotros</h1>
+        <script>
+            let stars = document.getElementById('star');
+            let moon = document.getElementById('moon');
+            let mountains_behind = document.getElementById('mountains_behind');
+            let text = document.getElementById('text');
+            let btn = document.getElementById('btn');
+            let mountains_front = document.getElementById('mountains_front');
 
-        <?php include 'iconos.php'; ?>
-    </section>
+            window.addEventListener('scroll', function() {
+                let value = window.scrollY;
+                stars.style.left = value * 0.25 + 'px';
+                moon.style.top = value * 1.05 + 'px';
+                mountains_behind.style.top = value * 0.5 + 'px';
+                mountains_front.style.top = value * 0 + 'px';
+                text.style.marginRight = value * 3 + 'px';
+                text.style.marginTop = value * 1.5 + 'px';
+                btn.style.marginTop = value * 1.5 + 'px';
+            })
+        </script>
