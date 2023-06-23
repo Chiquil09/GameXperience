7e0d94419cf9515643383311ce72e35bccc381ee
AyalaManu <114090237+AyalaManu@users.noreply.github.com>
2023-06-22 19:43:54 -0500

    22/06723-07:43

--- admin/crear.php
+++ admin/crear.php
@@ -10,7 +10,7 @@ if (!empty($_FILES['imagen']['name'])) {
 
     if (move_uploaded_file($_FILES['imagen']['tmp_name'], '../' . $rutaArchivo)) {
         $sql = "INSERT INTO productos (nombre, descripcion, imagen, precio) VALUES ('$nombre','$descripcion','$nombreArchivo',$precio)";
-        
+
         if ($mysqli->query($sql) === TRUE) {
             echo "El juego se creó correctamente.";
             header('Location: crear2.php');
@@ -24,37 +24,119 @@ if (!empty($_FILES['imagen']['name'])) {
 
 include("../includes/templates/header.php");
 ?>
-<section class="home-section">
-    <main class="contenedor container px-5">
-        <h1>Administrador de juegos</h1>
-        <a href="/admin/index.php" class="boton boton-verde">Volver</a>
+<!DOCTYPE html>
+<html>
 
-        <h2>Crear Juego</h2>
+<head>
+    <title>Título de tu página</title>
+    <style>
+        /* Estilos oscuros y elegantes */
+        body {
+            background-color: #222;
+            color: #fff;
+        }
 
-        <form method="POST" action="/admin/crear.php" enctype="multipart/form-data">
-            <table width="540">
-                <tr valign="top">
-                    <td width="500">Nombre</td>
-                    <td width="414"><input type="text" id="nombre" name="nombre" placeholder="Nombre"></td>
-                </tr>
+        .home-section {
+            padding: 50px 0;
+        }
 
-                <tr valign="top">
-                    <td>Descripción:</td>
-                    <td><textarea id="descripcion" name="descripcion" cols="110" rows="15"></textarea></td>
-                </tr>
+        .contenedor {
+            max-width: 800px;
+            margin: 0 auto;
+        }
 
-                <tr valign="top">
-                    <td>Imagen:</td>
-                    <td><input type="file" name="imagen"></td>
-                </tr>
+        h1 {
+            font-size: 36px;
+            margin-bottom: 30px;
+        }
 
-                <tr valign="top">
-                    <td width="500">Costo</td>
-                    <td width="414"><input type="number" name="precio" id="precio" placeholder="Precio del juego" maxlength="15"></td>
-                </tr>
-            </table>
-            <input name="restablecer" type="reset" id="restablecer" value="Restablecer">
-            <button type="submit" name="enviar" id="enviar">Registrarse</button>
-        </form>
-    </main>
-</section>
+        h2 {
+            font-size: 24px;
+            margin-bottom: 20px;
+        }
+
+        table {
+            width: 100%;
+            margin-bottom: 20px;
+        }
+
+        td {
+            padding: 10px;
+        }
+
+        input[type="text"],
+        input[type="number"],
+        textarea {
+            width: 100%;
+            padding: 10px;
+            border: none;
+            background-color: #555;
+            color: #fff;
+            border-radius: 4px;
+            margin-bottom: 10px;
+        }
+
+        textarea {
+            height: 150px;
+        }
+
+        input[type="file"] {
+            margin-bottom: 10px;
+        }
+
+        input[type="reset"],
+        button[type="submit"] {
+            padding: 10px 20px;
+            border: none;
+            background-color: #4CAF50;
+            color: #fff;
+            border-radius: 4px;
+            cursor: pointer;
+            transition: background-color 0.3s ease;
+        }
+
+        input[type="reset"]:hover,
+        button[type="submit"]:hover {
+            background-color: #45a049;
+        }
+    </style>
+</head>
+
+<body>
+    <section class="home-section">
+        <main class="contenedor container px-5">
+            <h1>Administrador de juegos</h1>
+            <a href="/admin/index.php" class="boton boton-verde">Volver</a>
+
+            <h2>Crear Juego</h2>
+
+            <form method="POST" action="/admin/crear.php" enctype="multipart/form-data">
+                <table width="540">
+                    <tr valign="top">
+                        <td width="500">Nombre</td>
+                        <td width="414"><input type="text" id="nombre" name="nombre" placeholder="Nombre"></td>
+                    </tr>
+
+                    <tr valign="top">
+                        <td>Descripción:</td>
+                        <td><textarea id="descripcion" name="descripcion" cols="110" rows="15"></textarea></td>
+                    </tr>
+
+                    <tr valign="top">
+                        <td>Imagen:</td>
+                        <td><input type="file" name="imagen"></td>
+                    </tr>
+
+                    <tr valign="top">
+                        <td width="500">Costo</td>
+                        <td width="414"><input type="number" name="precio" id="precio" placeholder="Precio del juego" maxlength="15"></td>
+                    </tr>
+                </table>
+                <input name="restablecer" type="reset" id="restablecer" value="Restablecer">
+                <button type="submit" name="enviar" id="enviar">Registrarse</button>
+            </form>
+        </main>
+    </section>
+</body>
+
+</html>
--- admin/crear2.php
+++ admin/crear2.php
@@ -1,65 +1,138 @@
 <?php
 include '../includes/funciones/db_conexion.php';
 
-if(!empty($_GET['puntuacion'])){
+if (!empty($_GET['puntuacion'])) {
     $puntuacion = $_GET['puntuacion'];
     $desarrollador = $_GET['desarrolador'];
     $genero_id = $_GET['genero_id'];
 
     $producto_id = $mysqli->query('SELECT id FROM productos ORDER BY id DESC');
-    $id=$producto_id->fetch_array(MYSQLI_ASSOC);
+    $id = $producto_id->fetch_array(MYSQLI_ASSOC);
 
-    $consulta = 'INSERT INTO datos(puntuacion, desarrolador, genero_id, producto_id) VALUES ('.$puntuacion.',"'. $desarrollador.'",'.$genero_id.', '.$id['id'].')';
-    
+    $consulta = 'INSERT INTO datos(puntuacion, desarrolador, genero_id, producto_id) VALUES (' . $puntuacion . ',"' . $desarrollador . '",' . $genero_id . ', ' . $id['id'] . ')';
+
     if ($mysqli->query($consulta) === TRUE) {
         echo "El juego se creó correctamente.";
         header('Location: /admin/');
     } else {
         echo "Error al crear el juego: " . $mysqli->error;
     }
-}else{
+} else {
 
     $sql = "SELECT * FROM generos";
     $result = $mysqli->query($sql);
 }
 include("../includes/templates/header.php");
 ?>
-<section class="home-section">
-    <main class="contenedor container px-5">
+<!DOCTYPE html>
+<html>
 
-        <h2>Datos Generales</h2>
+<head>
+    <title>Título de tu página</title>
+    <style>
+        /* Estilos tipo moderno con toque medieval y fondo oscuro */
+        body {
+            background-color: #222;
+            font-family: 'Arial', sans-serif;
+            color: #fff;
+        }
 
-        <form action="crear2.php">
-            <table width="540">
-                <tr valign="top">
-                    <td width="500">Puntuacion</td>
-                    <td width="414"><input type="number" id="puntuacion" name="puntuacion" placeholder="Puntuacion"></td>
-                </tr>
+        .home-section {
+            padding: 50px 0;
+        }
 
-                <tr valign="top">
-                    <td>Desarrollador:</td>
-                    <td><input type="text" id="desarrolador" name="desarrolador"></input></td>
-                </tr>
+        .contenedor {
+            max-width: 800px;
+            margin: 0 auto;
+        }
 
-                <tr valign="top">
-                    <td>Genero:</td>
-                    <td width="414">
-                        <select name="genero_id">
-                        <?php
-                            if ($result->num_rows > 0) {
-                                while($gen = $result->fetch_assoc()) {
-                                    ?>
-                                    <option value="<?php echo $gen['id']; ?>"><?php echo $gen['nombre']; ?></option>
-                                    <?php
+        h2 {
+            font-size: 28px;
+            margin-bottom: 20px;
+            border-bottom: 2px solid #fff;
+            padding-bottom: 10px;
+        }
+
+        table {
+            width: 100%;
+            margin-bottom: 20px;
+            border-collapse: collapse;
+        }
+
+        td {
+            padding: 10px;
+            border-bottom: 1px solid #888;
+        }
+
+        input[type="number"],
+        input[type="text"],
+        select {
+            width: 100%;
+            padding: 10px;
+            border: none;
+            background-color: #555;
+            color: #fff;
+            border-radius: 4px;
+            margin-bottom: 10px;
+        }
+
+        input[type="reset"],
+        button[type="submit"] {
+            padding: 10px 20px;
+            border: none;
+            background-color: #333;
+            color: #fff;
+            border-radius: 4px;
+            cursor: pointer;
+            transition: background-color 0.3s ease;
+        }
+
+        input[type="reset"]:hover,
+        button[type="submit"]:hover {
+            background-color: #666;
+        }
+    </style>
+</head>
+
+<body>
+    <section class="home-section">
+        <main class="contenedor container px-5">
+            <h2>Datos Generales</h2>
+
+            <form action="crear2.php">
+                <table width="540">
+                    <tr valign="top">
+                        <td width="500">Puntuacion</td>
+                        <td width="414"><input type="number" id="puntuacion" name="puntuacion" placeholder="Puntuacion"></td>
+                    </tr>
+
+                    <tr valign="top">
+                        <td>Desarrollador:</td>
+                        <td><input type="text" id="desarrolador" name="desarrolador"></input></td>
+                    </tr>
+
+                    <tr valign="top">
+                        <td>Genero:</td>
+                        <td width="414">
+                            <select name="genero_id">
+                                <?php
+                                if ($result->num_rows > 0) {
+                                    while ($gen = $result->fetch_assoc()) {
+                                ?>
+                                        <option value="<?php echo $gen['id']; ?>"><?php echo $gen['nombre']; ?></option>
+                                <?php
+                                    }
                                 }
-                            }
-                        ?>
-                        </select>
-                    </td>
-                </tr>
-            </table>
-            <input name="restablecer" type="reset" id="restablecer" value="Restablecer">
-            <button type="submit" name="enviar" id="enviar">Registrarse</button>
-        </form>
-    </main>
-</section>
+                                ?>
+                            </select>
+                        </td>
+                    </tr>
+                </table>
+                <input name="restablecer" type="reset" id="restablecer" value="Restablecer">
+                <button type="submit" name="enviar" id="enviar">Registrarse</button>
+            </form>
+        </main>
+    </section>
+</body>
+
+</html>
